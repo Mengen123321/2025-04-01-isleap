26e7349a8ffea71df53fc8b8752f5ae53ab0b242
Mengen123321 <mengenliu1@gmail.com>
2025-04-01 13:43:21 -0700

    finished

--- R/is_leap.R
+++ R/is_leap.R
@@ -1,12 +1,29 @@
 #' Is this a leap year?
 #'
-#' @param num a number
+#' @param num a number, or a stringn that costains a number
 #'
 #' @return TRUE or FALSE on whether or not it is a leap year
 #'
-#' @export
+#' @export 
+#' 
+#' @examples
+#' # example code
+#' is_leap(1000) # Returns TRUE
+#' is_leap(11) # Returns FALSE
+#' is_leap("the year is 1000 CE") # Returns TRUE
+#' 
+#' \dontrun{
+#' is_leap(mtcars)
+#' }
 #'
 is_leap <- function(num) {
+
+  if (is.character(num)) {
+    # Extract num from string
+    num <- stringr::str_extract(num, "\\d+")
+    num <- as.numeric(num)
+  }
+
   if (num %% 4 == 0) {
     return(TRUE)
   } else {
--- history.R
+++ history.R
@@ -18,22 +18,38 @@ library(devtools)
 
 usethis::use_git()
 
+# Create an R file for new package function
 usethis::use_r("is_leap")
 
+# Load package for usage, It roughly simulates what happens when a package is installed and loaded with library().
 devtools::load_all()
 
+# Use function
 is_leap(1000)
 
 devtools::check()
 
+# Implement Licence
 usethis::use_mit_license()
 
 devtools::check()
 
+# Build documentation (Can be rebuilt if function and spec changes)
+# oxygen2 will flesh out the .Rd file using data from the DESCRIPTION
 devtools::document()
 
 devtools::check()
 
+# Install packages for usage
 devtools::install()
 
 is_leap(1000)
+
+# Creates tests/testthat/, tests/testthat.R, and adds the testthat package to the Suggests field.
+usethis::use_testthat()
+
+# Run tests, runs script in testthat.R
+devtools::test()
+
+# adds a CRAN package dependency to DESCRIPTION and offers a little advice about how to best use it
+usethis::use_package("stringr")
